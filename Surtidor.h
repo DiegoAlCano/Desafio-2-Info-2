0927680cc2584d19991f51ffa770bd2af9956dd5
FelipeRodas <164042163+FelipeRodas@users.noreply.github.com>
2024-10-09 18:11:23 -0500

    creacion de clase

--- Surtidor.h
+++ Surtidor.h
@@ -1,1 +1,20 @@
+#ifndef SURTIDOR_H
+#define SURTIDOR_H
+#include <iostream>
+#include <string.h>
+using namespace std;
 
+
+class surtidor
+{
+    private://Atributos
+        string codigoIdentificador;
+        string modelo;
+        bool activo;
+    public://Metodos
+        surtidor(string, string, bool);//Constructor
+        ~surtidor();//Destructor
+        void setcambiarEstado(bool);
+};
+
+#endif // SURTIDOR_H

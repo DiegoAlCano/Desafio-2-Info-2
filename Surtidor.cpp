66d23681ca704205e34a0488ebbf1900028a9ba4
FelipeRodas <164042163+FelipeRodas@users.noreply.github.com>
2024-10-09 18:13:53 -0500

    declaracion de la clase

--- Surtidor.cpp
+++ Surtidor.cpp
@@ -1,1 +1,25 @@
+#include "surtidor.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+surtidor::surtidor(string _codigoIdentificador,string _modelo,bool _activo)
+{
+    codigoIdentificador = _codigoIdentificador;
+    modelo = _modelo;
+    activo = _activo;
+}
+
+void surtidor::cambiarEstado()
+{
+    if(activo==true)
+    {
+        activo= false;
+    }
+    else
+    {
+        activo=true;
+    }
+}
 

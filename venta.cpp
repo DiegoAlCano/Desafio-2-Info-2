134f5e26b103477961bdd96296e6d7235eb41ffb
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-10-10 18:00:23 -0500

    Update venta.cpp
    Se agrega el código de setters de cantidadDinero y cantidadCombistibleVendido

--- venta.cpp
+++ venta.cpp
@@ -1,84 +1,27 @@
 #include "venta.h"
 
-Venta::Venta() {
-
-    string fecha,hora,_fechaVenta,_horaVenta,_documentoCliente;
-    float _cantidadCombustibleVendido;
-    unsigned int _cantidadDinero;
-    short int tipoCombustible=0,tipoPago;
-
-
-    cout<<"Ingrese el dia de la venta: ";
-    cin>>fecha;
-    _fechaVenta += fecha;
-    _fechaVenta += '/';
-
-    cout<<"Ingrese el mes de la venta: ";
-    cin>>fecha;
-    _fechaVenta += fecha;
-    _fechaVenta += '/';
-
-    cout<<"Ingrese el anio de la venta: ";
-    cin>>fecha;
-    _fechaVenta += fecha;
+Venta::Venta(string _fechaVenta, string _horaVenta, float _cantidadCombustibleVendido, string _categoriaCombustible,
+             string _metodoPago, string _documentoCliente, unsigned int _cantidadDinero){
 
     fechaVenta = _fechaVenta;
-
-    cout<<"La hora de la venta se debe almacenar en formato 24 horas"<<endl;
-    cout<<"Ingrese la hora del dia: ";
-    cin>>hora;
-    _horaVenta += hora;
-    _horaVenta += ':';
-    cout<<"Ingrese el minuto del dia: ";
-    cin>>hora;
-    _horaVenta += hora;
-
     horaVenta = _horaVenta;
-
-    cout<<"Ingrese la cantidad de combustible vendido: ";
-    cin>>_cantidadCombustibleVendido;
-
     cantidadCombustibleVendido = _cantidadCombustibleVendido;
-
-    cout<<"Ingrese 1 para Combustible Regular, 2 para Premium y 3 para EcoExtra: ";
-    cin>>tipoCombustible;
-
-    if(tipoCombustible=='1'){
-        categoriaCombistible = "Regular";
-    }
-    else if(tipoCombustible=='2'){
-        categoriaCombistible = "Premium";
-    }
-
-    else{
-        categoriaCombistible = "EcoExtra";
-    }
-
-    cout<<"Ingrese 1 para pago en Efectivo, 2 para TDebito y 3 para TCredito: ";
-    cin>>tipoPago;
-
-    if(tipoPago=='1'){
-        metodoPago = "Efectivo";
-    }
-    else if(tipoPago=='2'){
-        metodoPago = "TDebito";
-    }
-
-    else{
-        metodoPago = "TCredito";
-    }
-
-    cout<<"Ingrese el numero de documento del cliente: ";
-    cin>>_documentoCliente;
-
+    categoriaCombustible = _categoriaCombustible;
+    metodoPago = _metodoPago;
     documentoCliente = _documentoCliente;
-
-    cout<<"Ingrese la cantidad de Dinero de la venta: ";
-    cin>>_cantidadDinero;
-
     cantidadDinero = _cantidadDinero;
 
 
+}
+
+Venta::Venta(){
+    fechaVenta="";
+    horaVenta="";
+    cantidadCombustibleVendido=0.0;
+    categoriaCombustible="";
+    metodoPago="";
+    documentoCliente="";
+    cantidadDinero=0;
 }
 
 float Venta::getCantidadCombustibleVendido() const{
@@ -86,18 +29,26 @@ float Venta::getCantidadCombustibleVendido() const{
 }
 
 string Venta::getCategoriaCombustible() const{
-    return categoriaCombistible;
+    return categoriaCombustible;
 }
 
 unsigned int Venta::getCantidadDinero() const{
     return cantidadDinero;
 }
 
+void Venta::setCantidadCombustibleVendido(float _cantidadCombustibleVendido){
+    cantidadCombustibleVendido = _cantidadCombustibleVendido;
+}
+
+void Venta::setCantidadDinero(unsigned int _cantidadDinero){
+    cantidadDinero = _cantidadDinero;
+}
+
 Venta::~Venta(){
     fechaVenta = "";
     horaVenta = "";
     cantidadCombustibleVendido = 0.0;
-    categoriaCombistible = "";
+    categoriaCombustible = "";
     metodoPago = "";
     documentoCliente = "";
     cantidadDinero = 0;
@@ -108,9 +59,10 @@ void Venta::mostrarVenta(){
     cout<<"Fecha: "<<fechaVenta<<endl;
     cout<<"Hora: "<<horaVenta<<endl;
     cout<<"Cantidad de combustible vendido: "<<cantidadCombustibleVendido<<endl;
-    cout<<"Categoria de combustible vendido: "<<categoriaCombistible<<endl;
+    cout<<"Categoria de combustible vendido: "<<categoriaCombustible<<endl;
     cout<<"Metodo de pago: "<<metodoPago<<endl;
     cout<<"Documento del cliente: "<<documentoCliente<<endl;
     cout<<"Valor de la venta: "<<cantidadDinero<<endl;
 }
+
 

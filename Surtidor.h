750a82f3ccc73c761cf4e02adc99cc8447d2b3c9
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-10-10 18:06:21 -0500

    Update Surtidor.h
    Se agregan nuevos atributos: numVentas es el numero de ventas registradas en el surtidor, ventas será un arreglo dinámico que almacenará objetos del tipo venta y capacidad que servirá como parámetro para  reservar memoria para el arreglo. Se agregan métodos como redimensionar que redimensiona el arreglo ventas, getters y setters de otros atributos

--- Surtidor.h
+++ Surtidor.h
@@ -2,19 +2,30 @@
 #define SURTIDOR_H
 #include <iostream>
 #include <string.h>
+#include "venta.h"
 using namespace std;
 
 
 class surtidor
 {
-    private://Atributos
-        string codigoIdentificador;
-        string modelo;
-        bool activo;
-    public://Metodos
-        surtidor(string, string, bool);//Constructor
-        ~surtidor();//Destructor
-        void setcambiarEstado(bool);
+private://Atributos
+    string codigoIdentificador;
+    string modelo;
+    bool activo;
+    int numVentas;     // Cantidad actual de ventas
+    int capacidad;     // Capacidad actual del arreglo
+    Venta *ventas;
+    void redimensionar();
+
+public://Metodos
+    surtidor(string, string, bool);//Constructor
+    surtidor();
+    ~surtidor();//Destructor
+    
+    string getCodigoIdentificador();
+    
+    void setcambiarEstado();
+    void agregarVenta(const Venta& nuevaVenta);
 };
 
 #endif // SURTIDOR_H

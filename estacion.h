e3ac9911bee603f873a500ee3c21e0667b3207d0
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-10-10 18:09:09 -0500

    Update estacion.h
    Se agregan atributos y métodos a la clase estacion

--- estacion.h
+++ estacion.h
@@ -8,18 +8,21 @@ using namespace std;
 
 class estacion
 {
-    private://Atributos
-        string nombre;
-        string identificador;
-        string gerente;
-        string region;
-        string ubicacionGPS;
-        surtidor Surtidores[12]={0};
-        int tanque[2][3];
-    public://Metodos
-        estacion(string, string, string, string);//Constructor
-        ~estacion();//Destructor
-        void setSurtidores(int indice, int valor);
+private://Atributos
+    string nombre;
+    string identificador;
+    string gerente;
+    string region;
+    string ubicacionGPS;
+    short int cantidadSurtidores;
+    surtidor *Surtidores;
+    float tanque[3][3] = {{0,1,2},{0,0,0},{0,0,0}};
+public://Metodos
+    estacion(string _nombre,string _identificador,string _gerente, string _region,string _ubicacionGPS);//Constructor
+    ~estacion();//Destructor
+    void agregarSurtidor(const surtidor& nuevoSurtidor);
+    void capacidadTanque();
+    void eliminarSurtidor();
 };
 
 #endif // ESTACION_H

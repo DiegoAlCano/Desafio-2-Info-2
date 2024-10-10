ac3d616d5c292f67a0c7652ceb4b26f064d38705
DiegoAlCano <144044835+DiegoAlCano@users.noreply.github.com>
2024-10-10 18:07:36 -0500

    Update Surtidor.cpp
    Se agrega código correspondiente a métodos, getters, setters

--- Surtidor.cpp
+++ Surtidor.cpp
@@ -1,14 +1,25 @@
 #include "surtidor.h"
 #include <iostream>
 #include <string.h>
+#include "venta.h"
 using namespace std;
 
+surtidor::surtidor(){
+    codigoIdentificador = "";
+    modelo = "";
+    activo = "";
+    capacidad = 0;
+    numVentas = 0;
+}
 
 surtidor::surtidor(string _codigoIdentificador,string _modelo,bool _activo)
 {
     codigoIdentificador = _codigoIdentificador;
     modelo = _modelo;
     activo = _activo;
+    capacidad = 15;
+    numVentas = 0;
+    ventas = new Venta[capacidad];
 }
 
 void surtidor::setcambiarEstado()
@@ -23,3 +34,33 @@ void surtidor::setcambiarEstado()
     }
 }
 
+string surtidor::getCodigoIdentificador(){
+    return codigoIdentificador;
+}
+
+void surtidor::redimensionar() {
+
+    int nuevaCapacidad = capacidad + 10;
+    Venta* nuevoArreglo = new Venta[nuevaCapacidad];
+
+    for (int i = 0; i<numVentas; i++) {
+        nuevoArreglo[i] = ventas[i];
+    }
+
+    delete[] ventas;
+
+    ventas = nuevoArreglo;
+    capacidad = nuevaCapacidad;
+}
+
+void surtidor::agregarVenta(const Venta& nuevaVenta) {
+    if (numVentas == capacidad) {
+        redimensionar();
+    }
+    ventas[numVentas] = nuevaVenta;
+    numVentas++;
+}
+
+surtidor::~surtidor(){
+    delete [] ventas;
+}

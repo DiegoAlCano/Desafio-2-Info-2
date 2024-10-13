851a7888950d8d2b157cbe4c35721650f7090382
FelipeRodas <164042163+FelipeRodas@users.noreply.github.com>
2024-10-13 12:13:19 -0500

    Update Main.cpp
    modificacion del menu

--- Main.cpp
+++ Main.cpp
@@ -16,65 +16,236 @@ int main()
     cin>> nombre;
     redNacional pais(nombre);
 
-    string mensaje = " Bienvenido a la red nacional de gasolineras de: " + nombre;
+    string mensaje = " Bienvenido a la red nacional de gasolineras de :" + nombre;
     imprimirTabla(mensaje);
     unsigned short int opc;
     do {
         string mensaje2 = "M E N U";
         imprimirTabla(mensaje2);
-        cout << " 1 Agregar una estacion"<< endl;
-        cout << " 2 Eliminar una estacion"<< endl;
-        cout << " 3 Calcular el monto total de las ventas en cada E/S del pais, discriminado por categoría de combustible."<< endl;
-        cout << " 4 Fijar los precios del combustible para toda la red."<< endl;
-        cout << " 5 Agregar un surtidor a una estacion"<< endl;
-        cout << " 6 Eliminar un surtidor de una  estacion"<< endl;
-        cout << " 7 Activar un surtidor de una estacion"<< endl;
-        cout << " 8 Desactivar un surtidor de una estacion"<< endl;
-        cout << " 9 Consultar el historico de transacciones de cada surtidor de la E/S"<< endl;
-        cout << " 10 Reportar la cantidad la cantidad de litros vendidos segun cada categoria de combustible" << endl;
-        cout << " 11 Simular una venta de combustible"<< endl;
-        cout << "12 Salir" << endl;
+        cout << " 1 Gestion de red "<< endl;
+        cout << " 2 Gestion de estacion de servicio"<< endl;
+        cout << " 3 Verificacion de fugas"<< endl;
+        cout << " 4 Salir" << endl;
         cout << "ELIGA LA OPCION QUE DESEE: "<< endl;
         cin >> opc;
 
         switch (opc) {
         case 1:
         {
-            cout << "Ha seleccionado la opcion  AGREGAR UNA ESTACION "<< endl;
-            string nombre1;
-            cout << "Ingrese el nombre de la estacion: ";
-            cin.ignore();
-            getline(cin,nombre1);
-            string codigo;
-            cout << "Ingrese el codigo identificador: ";
-            getline(cin,codigo);
-            string gerente;
-            cout << "Ingrese el nombre del gerente: ";
-            getline(cin,gerente);
-            string region;
-            while(true){
-                cout <<"Ingrese la region, SOLO SE PUEDE INGRESAR Norte, Sur o Centro: ";
-                getline(cin, region);
-                if (region == "Norte" || region == "Sur" || region == "Centro") {
-                    break;
-                } else {
-                    cout << "Entrada invalida. Por favor, ingrese una de las opciones permitidas." << endl;
-                }
+            unsigned short int opc1;
+            do{
+                string mensaje3 = "GESTION DE RED";
+                imprimirTabla(mensaje3);
+                cout << " 1 Agregar una estacion"<< endl;
+                cout << " 2 Eliminar una estacion"<< endl;
+                cout << " 3 Calcular el monto total de las ventas en cada E/S del pais, discriminado por categoria de combustible."<< endl;
+                cout << " 4 Fijar los precios del combustible para toda la red."<< endl;
+                cout << " 5 Volver al MENU anterior" << endl;
+                cout << "ELIGA LA OPCION QUE DESEE: "<< endl;
+                cin >> opc1;
+                switch (opc1) {
+                case 1:
+                {
+                    cout << "Ha seleccionado la opcion  AGREGAR UNA ESTACION "<< endl;
+                    string nombre1;
+                    cout << "Ingrese el nombre de la estacion: ";
+                    cin.ignore();
+                    getline(cin,nombre1);
+                    string codigo;
+                    cout << "Ingrese el codigo identificador: ";
+                    getline(cin,codigo);
+                    string gerente;
+                    cout << "Ingrese el nombre del gerente: ";
+                    getline(cin,gerente);
+                    string region;
+                    while(true){
+                        cout <<"Ingrese la region, SOLO SE PUEDE INGRESAR Norte, Sur o Centro: ";
+                        getline(cin, region);
+                        if (region == "Norte" || region == "Sur" || region == "Centro") {
+                            break;
+                        } else {
+                            cout << "Entrada invalida. Por favor, ingrese una de las opciones permitidas." << endl;
+                        }
 
-            }
-            region;
-            string ubicacionGPS;
-            cout << "Ingrese la ubicacion la Ubicacion GPS: ";
-            getline(cin,ubicacionGPS);
-            estacion estacion1(nombre1,codigo,gerente,region,ubicacionGPS);
-            pais.agregarEstacion(estacion1);
-            pais.mostrarEstaciones();
+                    }
+
+                    string ubicacionGPS;
+                    cout << "Ingrese la ubicacion la Ubicacion GPS: ";
+                    getline(cin,ubicacionGPS);
+                    estacion estacion1(nombre1,codigo,gerente,region,ubicacionGPS);
+                    pais.agregarEstacion(estacion1);
+                    pais.mostrarEstaciones();
+                    break;
+                }
+                case 2:
+                {
+                    cout << "Ha seleccionada la opcion ELIMINAR UNA ESTACION" << endl;
+                    cout << "Ingrese el nombre de la estacion: ";
+                    string nombre1;
+                    cin.ignore();
+                    getline(cin,nombre1);
+                    string codigo;
+                    cout << "Ingrese el codigo identificador: ";
+                    getline(cin,codigo);
+                    string gerente;
+                    cout << "Ingrese el nombre del gerente: ";
+                    getline(cin,gerente);
+                    string region;
+                    while(true){
+                        cout <<"Ingrese la region, SOLO SE PUEDE INGRESAR Norte, Sur o Centro: ";
+                        getline(cin, region);
+                        if (region == "Norte" || region == "Sur" || region == "Centro") {
+                            break;
+                        } else {
+                            cout << "Entrada invalida. Por favor, ingrese una de las opciones permitidas." << endl;
+                        }
+
+                    }
+
+                    string ubicacionGPS;
+                    cout << "Ingrese la ubicacion la Ubicacion GPS: ";
+                    getline(cin,ubicacionGPS);
+                    estacion estacion1(nombre1,codigo,gerente,region,ubicacionGPS);
+                    pais.eliminarEstacion(estacion1);
+                    pais.mostrarEstaciones();
+                    break;
+                }
+                case 3:
+                {
+                    cout << "Ha elegido la opcion Calcular el monto total de las ventas en cada E/S del pais, discriminado por categoria de combustible."<< endl;
+
+                    break;
+                }
+                case 4:
+                {
+                    cout << "Ha elegido la opcion Fijar los precios del combustible para toda la red."<< endl;
+
+                    break;
+                }
+                case 5:
+                {
+                    cout << "Saliendo de la opcion GESTION DE RED"<< endl;
+                    cout << "Volviendo al MENU anterior"<< endl;
+                    break;
+                }
+                default:
+                    cout << "Opción no válida.\n";
+                    break;
+                }
+              cout << endl;
+            }while(opc1 != 5);  // El ciclo continúa hasta que el usuario elija salir
 
             break;
         }
         case 2:
         {
-            cout << "Opción 2 seleccionada.\n";
+            unsigned short int opc1;
+            do{
+                string mensaje3 = "GESTION DE ESTACION DE SERVICIO";
+                imprimirTabla(mensaje3);
+                cout << " 1 Agregar un surtidor a una estacion"<< endl;
+                cout << " 2 Eliminar un surtidor de una  estacion"<< endl;
+                cout << " 3 Activar un surtidor de una estacion"<< endl;
+                cout << " 4 Desactivar un surtidor de una estacion"<< endl;
+                cout << " 5 Consultar el historico de transacciones de cada surtidor de la E/S"<< endl;
+                cout << " 6 Reportar la cantidad la cantidad de litros vendidos segun cada categoria de combustible" << endl;
+                cout << " 7 Simular una venta de combustible"<< endl;
+                cout << " 8 Volver al MENU anterior" << endl;
+                cout << "ELIGA LA OPCION QUE DESEE: "<< endl;
+                cin >> opc1;
+
+                switch (opc1) {
+                case 1:
+                {
+                    cout << "Ha seleccionado la opcion  AGREGAR UN SURTIDOR A LA ESTACION "<< endl;
+                    string nombre1;
+                    cout << "Ingrese el nombre de la ESTACION: ";
+                    cin.ignore();
+                    getline(cin,nombre1);
+                    string codigo;
+                    cout << "Ingrese el codigo identificador de la ESTACION: ";
+                    getline(cin,codigo);
+
+
+                    estacion estacion1(nombre1,codigo);
+                    estacion1 = pais.encontarEstacion(estacion1);
+                    cout<< "Ingrese el codigo del SURTIDOR: ";
+                    string codigoSurtidor;
+                    getline(cin,codigoSurtidor);
+                    cout<< "Ingrese el modelo del SURTIDOR: ";
+                    string modeloSurtidor;
+                    getline(cin,modeloSurtidor);
+                    string estado;
+                    while(true){
+                        cout<< "Ingrese el estado del SURTIDOR, true si esta ACTIVO o false si esta DESACTIVADO: ";
+                        getline(cin, estado);
+                        if (estado == "true" || estado == "false") {
+                            break;
+                        } else {
+                            cout << "Entrada invalida. Por favor, ingrese una de las opciones permitidas." << endl;
+                        }
+
+                    }
+                    bool ESTADO;
+                    if(estado== "true"){
+                        ESTADO = true;
+
+                    }
+                    else{
+                        ESTADO = false;
+                    }
+                    surtidor surtidor1(codigoSurtidor,modeloSurtidor,ESTADO);
+                    estacion1.agregarSurtidor(surtidor1);
+
+                    break;
+                }
+                case 2:
+                {
+                    cout << "Ha seleccionada la opcion ELIMINAR UN SURTIDOR DE LA ESTACION" << endl;
+                    string nombre1;
+                    cout << "Ingrese el nombre de la ESTACION: ";
+                    cin.ignore();
+                    getline(cin,nombre1);
+                    string codigo;
+                    cout << "Ingrese el codigo identificador de la ESTACION: ";
+                    getline(cin,codigo);
+                    estacion estacion1(nombre1,codigo);
+
+                    estacion1 = pais.encontarEstacion(estacion1);
+                    cout<< "Ingrese el codigo del SURTIDOR QUE DESEA ELIMINAR: ";
+                    string codigoSurtidor;
+                    getline(cin,codigoSurtidor);
+                    estacion1.eliminarSurtidor(codigoSurtidor);
+
+                    break;
+                }
+                case 3:
+                {
+
+                    cout << "Ha seleccionado la opcion ACTIVAR SURTIDOR"<< endl;
+                    break;
+                }
+                case 4:
+                {
+                    cout << "Ha seleccionado la opcion DESACTIVAR SURTIDOR"<< endl;
+                    break;
+                }
+                case 5:
+                {
+                    cout << "Saliendo de la opcion GESTION DE ESTACION DE SERVICIO"<< endl;
+                    cout << "Volviendo al MENU anterior"<< endl;
+                    break;
+                }
+                default:
+                    cout << "Opción no válida";
+                    break;
+                }
+                cout << endl;
+
+
+
+            }while(opc1 != 5);
+
             break;
         }
         case 3:
@@ -84,7 +255,7 @@ int main()
         }
         case 4:
         {
-            cout << "Opcion 4 seleccionada. \n";
+            cout << "Saliendo del programa.\n";
             break;
         }
         default:
@@ -94,7 +265,7 @@ int main()
 
         cout << endl;
 
-    } while (opc != 12);  // El ciclo continúa hasta que el usuario elija salir
+    } while (opc != 4);  // El ciclo continúa hasta que el usuario elija salir
 
     return 0;
 }
